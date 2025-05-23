5436b59e4f1bc65bd391f5bae43ef552e7197088
Adasupta <adasgupta+demo@qase.io>
2025-05-23 10:26:23 +0530

    Update test_tags.py

--- tests/examples/test_tags.py
+++ tests/examples/test_tags.py
@@ -1,5 +1,5 @@
 from qase.pytest import qase
 
-@qase.id(459)  # Replace 123 with your actual Qase test case ID
+#@qase.id(459)  # Replace 123 with your actual Qase test case ID
 def test_qase_tags():
     assert False, "This test is intentionally failing for demonstration"
